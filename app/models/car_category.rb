dd71b4a50f538269b0b314fda176007eaed9108f
Rogério Terciotte <rterciotte@gmail.com>
2020-08-13 15:50:32 -0300

    Commit 13-08-2020

--- app/models/car_category.rb
+++ app/models/car_category.rb
@@ -1,5 +1,5 @@
 class CarCategory < ApplicationRecord
     validates :name, :daily_rate, :car_insurance, 
-                :third_party_insurance, presence: { message: 'não pode ficar em branco' }
+                :third_party_insurance, presence: true
     validates :name, uniqueness: { case_sensitive: false }               
 end
