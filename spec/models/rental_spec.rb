edf25b2c04b02e2c1a30ebf6116d78dd66d0b3d6
Rogério Terciotte <rterciotte@gmail.com>
2020-08-24 16:31:42 -0300

    Geração de token da locação

--- spec/models/rental_spec.rb
+++ spec/models/rental_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Rental, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'token' do
+    it 'generate on create' do
+      client = Client.create!(name: 'Fulano Sicrano', email: 'client@test.com', cpf: '123.456.789-00')
+      car_category = CarCategory.create!(name: 'A', daily_rate: 1, car_insurance: 1, third_party_insurance: 1)
+      user = User.create!(name: 'Sicrano Fulano', email: 'user@test.com', password: '12345678' )
+      rental = Rental.new(start_date: Date.current, end_date: 1.day.from_now, user: user, client: client, car_category: car_category)
+
+      rental.save!
+      rental.reload
+
+      expect(rental.token).to be_present
+      expect(rental.token.size).to eq(6)
+      expect(rental.token).to match(/^[A-Z0-9]+$/)
+    end
+    
+    xit 'must be unique' do
+      
+    end   
+  end
 end
